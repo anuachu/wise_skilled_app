05da66263568cf07b4d630beae89de4dea30824a
anuachu <anushamohan148@gmail.com>
2022-10-04 23:29:12 +1030

    Added features for sign_up, login,error messages

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const PORT = process.env.PORT || 3000;
 
 //Controllers
 const profilesController = require('./controllers/profiles_controller')
+const usersController = require('./controllers/users_controller')
+const sessionsController = require('./controllers/sessions_controller')
 const logger = require('./middlewares/logger')
+const sessions = require('./middlewares/sessions')
 
 app.listen(PORT,
   () => console.log(`server listening to port ${PORT}`)
@@ -13,5 +16,8 @@ app.listen(PORT,
 app.use(logger)
 app.use(express.static('client'))
 app.use(express.json())
+app.use(sessions)
 
 app.use('/api/profiles', profilesController)
+app.use('/api/users', usersController)
+app.use('/api/sessions', sessionsController)
