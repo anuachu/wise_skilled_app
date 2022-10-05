4b49e736794237e5252fc858dfe87dd72854b297
anuachu <anushamohan148@gmail.com>
2022-10-06 09:15:49 +1030

    password-length feature

--- client/js/components/sign_up.js
+++ client/js/components/sign_up.js
@@ -13,9 +13,9 @@ function renderSignUp() {
         </fieldset>
         <fieldset>
           <label for="">Password: </label>
-          <input type="password" name="password">
+          <input type="password" name="password" id="textbox"  >
         </fieldset>
-        <button>Sign Up</button>
+        <button onClick="checkLength()" >Sign Up</button>
       </form>
     </section>
   `
@@ -38,3 +38,18 @@ function signUp(event) {
       renderProfileList()
     })
 }
+
+function checkLength(errorMessage){
+  var textbox = document.getElementById("textbox");
+  if(textbox.value.length <= 10 && textbox.value.length >= 5){
+      alert("success");
+  }
+  else{
+      alert("make sure the input is between 5-10 characters long")
+      textbox.innerHTML =
+      `<h2 style='color: red;'>${errorMessage}</h2>` +
+      textbox.innerHTML
+      renderSignUp()
+  }
+}
+
