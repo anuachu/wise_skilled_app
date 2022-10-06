954d32012cb78fb72dda14a6289a6ad34d513af2
jasongriggs <jay.griggs86@gmail.com>
2022-10-06 11:53:03 +1030

    Minor Tweaks to Scehma

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,8 @@ CREATE TABLE profiles(
   location TEXT,
   profile_img TEXT,
   rate REAL,
-  contact TEXT
+  contact TEXT,
+  author TEXT
 );
 
 
@@ -23,8 +24,6 @@ INSERT INTO profiles(headline, skills_summary, location, profile_img, rate, cont
 VALUES
 ('Retired Gardener', 'Retired gardener with expertise in lawn maintainence, weed control and roses', 'Salisbury, SA', 'https://images.pexels.com/photos/169523/pexels-photo-169523.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 23.50, '0468344566', 'test'),
 
-('Painter looking for more work', 'Painter looking to slow down and do smaller jobs, have 25 ywears experience acorss all areas.', 'Glenelg, SA', 'https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 21.25, '0468377898', 'test');
+('Painter looking for more work', 'Painter looking to slow down and do smaller jobs, have 25 ywears experience acorss all areas.', 'Glenelg, SA', 'https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 21.25, '0468377898', 'test'),
 
-
-ALTER TABLE profiles
-  ADD author TEXT;
+('Mechanic with some free time.', 'Looking to do a few small jobs while i am semi retired.', 'Glenelg, SA', 'https://images.unsplash.com/photo-1567093322503-341d262ad8f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 29.80, '0468370099', 'test');
