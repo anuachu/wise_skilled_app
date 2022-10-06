b3a70fb1cb7bb859f0b1a95d88496d3b46a87340
anuachu <anushamohan148@gmail.com>
2022-10-06 11:52:35 +1030

    password length changes

--- client/js/components/sign_up.js
+++ client/js/components/sign_up.js
@@ -5,17 +5,17 @@ function renderSignUp() {
         <h2>Sign Up:</h2>
         <fieldset>
           <label for="">Name: </label>
-          <input type="text" name="name">
+          <input type="text" placeholder="Name" name="name">
         </fieldset>
         <fieldset>
           <label for="">Email: </label>
-          <input type="text" name="email">
+          <input type="text" placeholder="Email" name="email">
         </fieldset>
         <fieldset>
           <label for="">Password: </label>
-          <input type="password" name="password" id="textbox"  >
+          <input type="password" placeholder="Password" name="password" id="textbox">
         </fieldset>
-        <button onClick="checkLength()" >Sign Up</button>
+        <button >Sign Up</button>
       </form>
     </section>
   `
@@ -34,22 +34,24 @@ function signUp(event) {
   })
     .then(res => res.json())
     .then(userName => {
+      if(userName.error) {
+        renderSignUp()
+        renderError(userName.error)
+      }else{
       state.loggedInUserName = userName
       renderProfileList()
+      }
     })
+  }
+
+
+function renderError(errorMessage){
+  const page = document.querySelector('#page')
+  page.innerHTML =
+    `<h2 style='color: red;'>${errorMessage}</h2>` +
+    page.innerHTML
 }
 
-function checkLength(errorMessage){
-  var textbox = document.getElementById("textbox");
-  if(textbox.value.length <= 10 && textbox.value.length >= 5){
-      alert("success");
-  }
-  else{
-      alert("make sure the input is between 5-10 characters long")
-      textbox.innerHTML =
-      `<h2 style='color: red;'>${errorMessage}</h2>` +
-      textbox.innerHTML
-      renderSignUp()
-  }
-}
+
+
 
