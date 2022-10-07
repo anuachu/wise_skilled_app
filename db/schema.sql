1e49a7b8ac4a9c4433683783ebcd0dc9c907b79d
jasongriggs <jay.griggs86@gmail.com>
2022-10-07 13:37:08 +1030

    heroku final

--- db/schema.sql
+++ db/schema.sql
@@ -22,8 +22,8 @@ CREATE TABLE profiles(
 
 INSERT INTO profiles(headline, skills_summary, location, profile_img, rate, contact, author)
 VALUES
-('Retired Gardener', 'Retired gardener with expertise in lawn maintainence, weed control and roses', 'Salisbury, SA', 'https://images.pexels.com/photos/169523/pexels-photo-169523.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 23.50, '0468344566', 'test'),
+('Sewing Teacher', 'I am specialist in Restoring wood furniture, Wood polishes, conditioners, and refinishing products can revitalize a tired, faded wood surface.
+', 'Salisbury, SA', 'https://plus.unsplash.com/premium_photo-1661495472526-2b9430880690?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', 30, '0468344566', 'DemoUser'),
 
-('Painter looking for more work', 'Painter looking to slow down and do smaller jobs, have 25 ywears experience acorss all areas.', 'Glenelg, SA', 'https://images.pexels.com/photos/1669754/pexels-photo-1669754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 21.25, '0468377898', 'test'),
+('Furniture restorer', 'am specialist in Restoring wood furniture, Wood polishes, conditioners, and refinishing products can revitalize a tired, faded wood surface', 'Glenelg, SA', 'https://images.unsplash.com/photo-1567615882203-818c7a0508e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=866&q=80', 30, '0468377898', 'DemoUser');
 
-('Mechanic with some free time.', 'Looking to do a few small jobs while i am semi retired.', 'Glenelg, SA', 'https://images.unsplash.com/photo-1567093322503-341d262ad8f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 29.80, '0468370099', 'test');
